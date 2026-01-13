086bfc9b8fc72998ffc6b9eb6bb300565f45f578
Diego <didipinheiro@outlook.com>
2026-01-12 21:22:43 -0300

    add: implement random text feature and update modal settings

--- src/components/mainForm.tsx
+++ src/components/mainForm.tsx
@@ -1,14 +1,27 @@
 import { useState } from 'react'
-import { Gear, GearIcon, XIcon } from '@phosphor-icons/react'
-import { Dialog } from 'radix-ui'
+import { DiceFiveIcon, GearIcon, XIcon } from '@phosphor-icons/react'
+import {
+  Dialog,
+  DialogBackdrop,
+  DialogPanel,
+  DialogTitle
+} from '@headlessui/react'
 
 interface Props {
   submitForm: () => void
   getText: (text: string) => void
 }
 
+import texts from '../stories.json'
+
 function MainForm(props: Props) {
   const [text, setText] = useState<string>('')
+
+  const randomText = () => {
+    const randomIndex = Math.floor(Math.random() * texts.length)
+    setText(texts[randomIndex])
+		props.getText(texts[randomIndex])
+  }
 
   return (
     <div className="flex w-screen h-screen items-center justify-center select-none">
@@ -19,7 +32,7 @@ function MainForm(props: Props) {
           name="input"
           rows={15}
           cols={70}
-          className="bg-emerald-200 outline-none px-4 py-2 text-2xl rounded-md resize-none"
+          className="bg-emerald-200 focus:ring-2 ring-emerald-100 outline-none px-4 py-4 text-2xl rounded-md resize-none transition"
           value={text}
           onChange={e => {
             props.getText(e.target.value)
@@ -29,86 +42,108 @@ function MainForm(props: Props) {
 
         <div className="flex gap-4 w-full">
           <button
+            onClick={randomText}
+            className="bg-emerald-500 text-white p-4 rounded-md cursor-pointer flex justify-center items-center transition hover:bg-emerald-600 outline-none border-none focus:ring-2 ring-emerald-200 "
+          >
+            <DiceFiveIcon size={32} color="white" />
+          </button>
+          <button
             onClick={() => {
               props.getText(text)
               props.submitForm()
             }}
-            className="bg-emerald-500 text-white p-4 rounded-md cursor-pointer flex justify-center items-center w-full transition hover:bg-emerald-600"
+            className="bg-emerald-500 text-white p-4 rounded-md cursor-pointer flex justify-center items-center w-full transition hover:bg-emerald-600 outline-none focus:ring-2 ring-emerald-200"
           >
             Start
           </button>
 
-          <button className="bg-emerald-500 text-white p-4 rounded-md cursor-pointer flex justify-center items-center transition hover:bg-emerald-600">
-            <GearIcon size={32} color="white" />
-          </button>
+          <Modal />
         </div>
       </div>
     </div>
   )
 }
 
-// function Modal() {
-//   return (
-// 	<Dialog.Root>
-// 		<Dialog.Trigger asChild>
-// 			<button className="items-center justify-center rounded bg-emerald-500 py-2 px-4 outline-none outline-offset-1 hover:bg-emerald-600 focus-visible:outline-2 focus-visible:outline-emerald-700 cursor-pointer">
-// 				<GearIcon size={32}  color='white' />
-// 			</button>
-// 		</Dialog.Trigger>
-// 		<Dialog.Portal>
-// 			<Dialog.Overlay className="fixed inset-0 bg-blackA6 data-[state=open]:animate-overlayShow" />
-// 			<Dialog.Content className="fixed left-1/2 top-1/2 max-h-[85vh] w-[90vw] max-w-[500px] -translate-x-1/2 -translate-y-1/2 rounded-md bg-gray1 p-[25px] shadow-(--shadow-6) focus:outline-none data-[state=open]:animate-contentShow">
-// 				<Dialog.Title className="m-0 text-[17px] font-medium text-mauve12">
-// 					Edit profile
-// 				</Dialog.Title>
-// 				<Dialog.Description className="mb-5 mt-2.5 text-[15px] leading-normal text-mauve11">
-// 					Make changes to your profile here. Click save when you're done.
-// 				</Dialog.Description>
-// 				<fieldset className="mb-4 flex items-center gap-5">
-// 					<label
-// 						className="w-22 text-right text-xl text-violet11"
-// 						htmlFor="name"
-// 					>
-// 						Name
-// 					</label>
-// 					<input
-// 						className="inline-flex h-[35px] w-full flex-1 items-center justify-center rounded px-2.5 text-[15px] leading-none text-violet11 shadow-[0_0_0_1px] shadow-violet7 outline-none focus:shadow-[0_0_0_2px] focus:shadow-violet8"
-// 						id="name"
-// 						defaultValue="Pedro Duarte"
-// 					/>
-// 				</fieldset>
-// 				<fieldset className="mb-[15px] flex items-center gap-5">
-// 					<label
-// 						className="w-[90px] text-right text-[15px] text-violet11"
-// 						htmlFor="username"
-// 					>
-// 						Username
-// 					</label>
-// 					<input
-// 						className="inline-flex h-[35px] w-full flex-1 items-center justify-center rounded px-2.5 text-[15px] leading-none text-violet11 shadow-[0_0_0_1px] shadow-violet7 outline-none focus:shadow-[0_0_0_2px] focus:shadow-violet8"
-// 						id="username"
-// 						defaultValue="@peduarte"
-// 					/>
-// 				</fieldset>
-// 				<div className="mt-[25px] flex justify-end">
-// 					<Dialog.Close asChild>
-// 						<button className="inline-flex h-[35px] items-center justify-center rounded bg-green4 px-[15px] font-medium leading-none text-green11 outline-none outline-offset-1 hover:bg-green5 focus-visible:outline-2 focus-visible:outline-green6 select-none">
-// 							Save changes
-// 						</button>
-// 					</Dialog.Close>
-// 				</div>
-// 				<Dialog.Close asChild>
-// 					<button
-// 						className="absolute right-2.5 top-2.5 inline-flex size-[25px] appearance-none items-center justify-center rounded-full text-violet11 bg-gray3 hover:bg-violet4 focus:shadow-[0_0_0_2px] focus:shadow-violet7 focus:outline-none"
-// 						aria-label="Close"
-// 					>
-// 						<XIcon size={32} />
-// 					</button>
-// 				</Dialog.Close>
-// 			</Dialog.Content>
-// 		</Dialog.Portal>
-// 	</Dialog.Root>
-//   )
-// }
+function Modal() {
+  const [open, setOpen] = useState(false)
+
+  const [wpm, setWpm] = useState(200)
+
+  function onWpmChange(newWpm: string) {
+    if (isNaN(Number(newWpm))) return
+
+    setWpm(Math.min(Number(newWpm), 9999))
+  }
+
+  return (
+    <div>
+      <button
+        onClick={() => setOpen(true)}
+        className="bg-emerald-500 text-white p-4 rounded-md cursor-pointer flex justify-center items-center transition hover:bg-emerald-600 outline-none border-none focus:ring-2 ring-emerald-200"
+      >
+        <GearIcon size={32} color="white" />
+      </button>
+      <Dialog open={open} onClose={setOpen} className="relative z-10">
+        <DialogBackdrop
+          transition
+          className="fixed inset-0 bg-gray-900/50 transition-opacity data-closed:opacity-0 data-enter:duration-300 data-enter:ease-out data-leave:duration-200 data-leave:ease-in"
+        />
+
+        <div className="fixed inset-0 z-10 w-screen overflow-y-auto">
+          <div className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
+            <DialogPanel
+              transition
+              className="relative transform overflow-hidden rounded-lg bg-gray-800 text-left shadow-xl outline -outline-offset-1 outline-white/10 transition-all data-closed:translate-y-4 data-closed:opacity-0 data-enter:duration-300 data-enter:ease-out data-leave:duration-200 data-leave:ease-in sm:my-8 sm:w-full sm:max-w-lg data-closed:sm:translate-y-0 data-closed:sm:scale-95"
+            >
+              <div className="bg-gray-800 px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
+                <div className="sm:flex sm:items-start">
+                  <div className="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">
+                    <DialogTitle
+                      as="h3"
+                      className="text-base font-semibold text-white"
+                    >
+                      Settings
+                    </DialogTitle>
+                    <div className="mt-2 flex gap-4 items-center">
+                      <input
+                        type="text"
+                        value={wpm}
+                        onChange={e => onWpmChange(e.target.value)}
+                        name="wpm"
+                        id="wpm"
+                        max={9999}
+                        className="bg-gray-500 w-20 text-gray-200 px-4 py-2 outline-none rounded-md"
+                      />
+                      <label htmlFor="wpm" className="text-xl text-gray-200">
+                        Words per minute
+                      </label>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className="bg-gray-700/25 px-4 py-3 sm:flex sm:flex-row-reverse sm:px-6">
+                <button
+                  type="button"
+                  onClick={() => setOpen(false)}
+                  className="inline-flex w-full justify-center rounded-md bg-emerald-500 px-3 py-2 text-sm font-semibold text-gray-100 hover:bg-emerald-600 cursor-pointer sm:ml-3 sm:w-auto"
+                >
+                  Save
+                </button>
+                <button
+                  type="button"
+                  data-autofocus
+                  onClick={() => setOpen(false)}
+                  className="mt-3 inline-flex w-full justify-center rounded-md bg-white/10 px-3 py-2 text-sm font-semibold text-white inset-ring inset-ring-white/5 hover:bg-white/20 sm:mt-0 sm:w-auto"
+                >
+                  Cancel
+                </button>
+              </div>
+            </DialogPanel>
+          </div>
+        </div>
+      </Dialog>
+    </div>
+  )
+}
 
 export default MainForm
